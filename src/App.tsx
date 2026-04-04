a51a317a282fc48d369025976242d90086f2a9a9
demouser99-max <vootakayalu@gmail.com>
2026-04-04 09:58:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-```tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -73,4 +73,3 @@ const App = () => (
 );
 
 export default App;
-```
